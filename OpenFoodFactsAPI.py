16d1d8ee312e078e2859af015e7a0208cfb9954b
Tiffany Tam <itsteep@umich.edu>
2023-12-02 16:38:36 -0500

    yelp and open

--- OpenFoodFactsAPI.py
+++ OpenFoodFactsAPI.py
@@ -1,28 +1,86 @@
 # Gather data from Open Food Facts API
+import json
 import requests
 import sqlite3 
 import pandas as pd 
 
-def gather_open_food_data(barcode):
-    open_food_url = f"https://world.openfoodfacts.net/api/v2/product/{barcode}" # Input barcode to fetch data dynamically
-    response = requests.get(open_food_url)
-    data = response.json()
-    save_data_to_db('open_food', [data.get('product', {})])
+import os
 
-def save_data_to_db(source, data):
-    conn = sqlite3.connect('fastfood.sqlite')
+DBNAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'OpenFoodFacts.db')
+
+DBNAME = 'OpenFoodFacts.db'
+    
+def create_food_db():
+    conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
 
-    if source == 'open_food':
-        for product in data:
-            product_name = product.get('product_name')
-            ecoscore = product.get('ecoscore_grade')
-            if product_name and ecoscore:  # we want both fields to be not None
-                cur.execute('INSERT INTO OpenFood (product_name, ecoscore) VALUES (?, ?)', (product_name, ecoscore))
+    # Creating food details' table
+    create_foods_tbl = '''
+        CREATE TABLE IF NOT EXISTS 'Foods' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Name' TEXT,
+            'Brand' TEXT,
+            'EcoScore' INTEGER
+        );
+    '''
+    cur.execute(create_foods_tbl)
 
+    conn.commit()
+    conn.close()
+
+def add_food_data():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    base_url = "https://world.openfoodfacts.org/api/v0/product/"
+    
+    # Sample product barcodes
+    product_barcodes = ['3017620422003', '3502110009449', '87157215', '8000500037560']
+
+    for barcode in product_barcodes:
+        params = {'barcode': barcode}
+        response = requests.get(base_url + barcode + '.json', params=params)
+        result = response.json()
+        
+        if 'product' in result:
+            product = result['product']
+            name = product['product_name'] if 'product_name' in product else "No Data"
+            brand = product['brands'] if 'brands' in product else "No Data"
+            eco_score = product['ecoscore_grade'] if 'ecoscore_grade' in product else "No Data"
+
+            insertion = (None,name,brand,eco_score)
+
+            statement = 'INSERT INTO "Foods"'
+            statement += 'VALUES (?,?,?,?)'
+            cur.execute(statement, insertion)
+
+    conn.commit()
+    conn.close()
+
+def get_food_data():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    statement = "SELECT Name, Brand, EcoScore FROM Foods"
+    cur.execute(statement)
+
+    data = cur.fetchall()
+    
+    return data
+
+# Creating DB & Tables
 def main():
-    barcodes = ['3017624010701', '737628064502'] # list of barcodes to be fetched
-    for barcode in barcodes:
-        gather_open_food_data(barcode)
+    create_food_db()
+
+# Populating tables with data from openfoodfacts API
+    add_food_data() 
+
+# Retrieveing and printing data from the table
+    get_food_data()
 
 main()
+
+
+
+
+
--- YelpAPI.py
+++ YelpAPI.py
@@ -1,1 +1,68 @@
 # Gather data from Yelp API
+# Gather data from Yelp API
+import requests
+import sqlite3
+import json
+
+yelp_searches = 'https://api.yelp.com/v3/businesses/search?location=Ann%20Arbor'
+yelp_reviews = 'https://api.yelp.com/v3/businesses/{id}/reviews'
+
+yelp_api = 'olWZg2mrP64E83paLsch77dR1jZLQ4jrMq271D_Kqt-gqf2qw1xlukFYMqVtK6s5TzKNqY-6lH7S1RWFeSjWt0cLdYSxkdK4UNCtK1lcEbOrO8rqDzO3oCBNv7RqZXYx'
+
+YELP_DB = "Yelp.db"
+
+def create_yelp_db():
+    conn = sqlite3.connect(YELP_DB)
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS YelpData (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            rating REAL,
+            reviews TEXT)
+    """)
+    conn.commit()
+    conn.close()
+
+def gather_yelp_data(keyword):
+    parameter = {
+        'term': keyword,
+        'categories': 'restaurants',
+        'limit': 50,
+        'api_key': yelp_api,
+    }
+
+    response = requests.get(yelp_searches, headers={'Authorization': f'Bearer {yelp_api}'}, params=parameter)
+    data = response.json()
+
+    for restaurant in data.get('businesses', []):
+        restaurant_id = restaurant.get('id')
+        reviews_response = requests.get(yelp_reviews.format(id=restaurant_id), headers={'Authorization': f'Bearer {yelp_api}'})
+        reviews_data = reviews_response.json()
+
+        save_data_to_db('yelp_data', restaurant, reviews_data.get('reviews', []))
+
+def save_data_to_db(source, restaurant_data, reviews_data):
+    conn = sqlite3.connect(YELP_DB)
+    cur = conn.cursor()
+
+    if source == 'yelp_data':
+        name = restaurant_data.get('name')
+        rating = restaurant_data.get('rating')
+        reviews = []
+        for review in reviews_data:
+            review_text = review.get('text')
+            if review_text:
+                reviews.append(review_text)
+        if name and rating:
+            cur.execute('INSERT INTO YelpData (name, rating, reviews) VALUES (?, ?, ?)', (name, rating, json.dumps(reviews)))
+            conn.commit()
+
+    conn.close()
+
+def main():
+    create_yelp_db()
+    gather_yelp_data('restaurants')  # You can specify a different keyword here if needed
+
+main()
